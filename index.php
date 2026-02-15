714ffef154371afc2949c169a78e0a5f334fde87
joel06rob <joel.roberts2006@gmail.com>
2026-02-15 16:48:38 +0000

    first

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Watch Shop</title>
+    <!-- Tailwind CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="bg-[#161616]">
+
+<!-- NAVBAR -->
+<nav class="absolute top-0 left-0 w-full bg-none z-10">
+    <div class="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+        <h1 class="text-2xl text-white font-semibold">JR Timepieces</h1>
+
+        <ul class="flex space-x-6 text-lg text-white">
+            <li><a href="#products" class="hover:text-[#BFB578]">Products</a></li>
+            <li><a href="cart.php" class="hover:text-[#BFB578]">Cart</a></li>
+        </ul>
+    </div>
+</nav>
+
+<!-- HERO SECTION -->
+<section class="bg-gradient-to-b from-[#161616] to-[#242424]  flex items-center justify-center">
+</section>
+
+<!-- PRODUCT LIST -->
+<div id="products" class="max-w-7xl mx-auto py-20 pb-28 px-4">
+
+    <h2 class="text-3xl font-bold mb-5 text-center text-white">Our Collection</h2>
+    <h3 class="text-2xl font-regular mb-20 text-center text-[#BFB578]">Explore our finest timepieces</h2>
+    
+    <h4 class="text-3xl font-semibold mb-5 text-white">Tailor Your Pick</h4>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+    </div>
+</div>
+<section class="bg-white">
+<div class="max-w-4xl mx-auto py-16 px-10 grid grid-cols-2 gap-20">
+<p class="py-10">JR Timepieces has been established since the 1800's. We have been carefully assembling timepieces in a professional, expert manner for as long as we can remember. Not one bezel or hand hasn't been carefully crafted by our expert watchmakers. Our watches are like no other, we only craft for the best.</p>
+<img src="media/images.jpeg">
+</div>
+</section>
+
+<footer class="flex text-white font-medium py-8 px-8 gap-6">
+    <a href="#">Contact Us</a>
+    <a href="#">Make a Purchase</a>
+    <a href="cart.php">Your Cart</a>
+</footer>
+
+</body>
+</html>
