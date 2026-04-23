a6c1b94194a6ab55aaa2ee2e0034a46da1c43f4a
joel06rob <joel.roberts2006@gmail.com>
2026-04-23 19:31:03 +0100

    admin - navigation

--- index.php
+++ index.php
@@ -34,8 +34,10 @@
 
             <?php if($auth->checkUser()): ?>
 
-                <a href="" class="block px-4 py-2 rounded-lg hover:bg-gray-100">Profile</a>
-                <a href="" class="block px-4 py-2 rounded-lg hover:bg-gray-100">Settings</a>
+                <?php if($auth->checkAdmin()): ?>
+                    <a href="admin_manage.php" class="block px-4 py-2 rounded-lg hover:bg-gray-100">Manage (Admin)</a>
+                <?php endif; ?>
+                <a href="orders.php" class="block px-4 py-2 rounded-lg hover:bg-gray-100">Orders</a>
                 <a href="logout.php" class="block px-4 py-2 rounded-lg hover:bg-gray-100">Logout</a>
 
             <?php else: ?>
